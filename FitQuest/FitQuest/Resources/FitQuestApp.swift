cf1d2d885e676a6b9d0cedc790fd878cbf42e55b
Ethan Lukas <62733963+EthanLukas@users.noreply.github.com>
2024-02-08 19:41:03 -0800

    Add files via upload
    Ethan's 1st Commit

---
+++ FitQuest/FitQuest/Resources/FitQuestApp.swift
@@ -0,0 +1,20 @@
+//
+//  FitQuestApp.swift
+//  FitQuest
+//
+//  Created by Ethan Lukas on 2/7/24.
+//
+
+import SwiftUI
+
+@main
+struct FitQuestApp: App 
+{
+    var body: some Scene
+    {
+        WindowGroup 
+        {
+            DemoView()
+        }
+    }
+}
