3a0d604fed2af85956996fe3590712e7644b7a1a
Ethan Lukas <ethanalukas@gmail.com>
2024-02-24 23:56:49 -0800

    Sleep Front End Finished

--- FitQuest/FitQuest/SleepView.swift
+++ FitQuest/FitQuest/SleepView.swift
@@ -11,6 +11,8 @@ struct SleepView: View
 {
     @State private var selectedHour = 7 // Default hour
     @State private var selectedMinute = 0 // Default minute
+    
+    @State private var selectedTime = Date()
 
     var body: some View 
     {
@@ -21,25 +23,44 @@ struct SleepView: View
                 .multilineTextAlignment(.center)
                 .padding()
             
-            HStack
-            {
-                Picker(selection: $selectedHour, label: Text(""))
-                {
-                    ForEach(0..<24, id: \.self)
-                    { hour in
-                        Text("\(hour)").tag(hour)
-                    }
-                }
-                .labelsHidden()
-                
-                Picker(selection: $selectedMinute, label: Text(""))
-                {
-                    ForEach(0..<60, id: \.self) { minute in
-                        Text(String(format: "%02d", minute)).tag(minute)
-                    }
-                }
-                .labelsHidden()
+//            HStack
+//            {
+//                Picker(selection: $selectedHour, label: Text(""))
+//                {
+//                    ForEach(0..<24, id: \.self)
+//                    { hour in
+//                        Text("\(hour)").tag(hour)
+//                    }
+//                }
+//                .labelsHidden()
+//                
+//                Picker(selection: $selectedMinute, label: Text(""))
+//                {
+//                    ForEach(0..<60, id: \.self) { minute in
+//                        Text(String(format: "%02d", minute)).tag(minute)
+//                    }
+//                }
+//                .labelsHidden()
+//            }
+            
+            DatePicker("", selection: $selectedTime, displayedComponents: .hourAndMinute)
+                            .datePickerStyle(WheelDatePickerStyle())
+                            .labelsHidden()
+                            .padding()
+            
+            Button(action: {
+                // Action to get bedtime
+            }) {
+                Text("Get My Personalized BedTime")
+                    .foregroundColor(.white)
+                    .padding()
+                    .frame(maxWidth: 250)
+                    .background(Color.blue)
+                    .cornerRadius(10)
             }
+            
+            
+            
         }
     }
 }
